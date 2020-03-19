b90a84cd8fb552fd27a19a922255ab8149c51a97
tongnamuu <33937684+thban33@users.noreply.github.com>
2020-03-19 18:26:09 +0900

    Middleware

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+function custommiddleware(req, res, next) {
+  console.log("Custom Middleware with Error");
+  next(new Error("Error"));
+}
+function errormiddleware(error, req, res, next) {
+  console.log(error.message);
+  next();
+}
+app.use(custommiddleware);
+app.use(errormiddleware);
+app.use(morgan("dev"));
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () =>
