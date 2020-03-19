4fbd35fc19bd64a77c906d6cfa379f46a84ee145
tongnamuu <33937684+thban33@users.noreply.github.com>
2020-03-19 19:01:57 +0900

    router added

--- index.js
+++ index.js
@@ -3,18 +3,9 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
-function custommiddleware(req, res, next) {
-  console.log("Custom Middleware with Error");
-  next(new Error("Error"));
-}
-function errormiddleware(error, req, res, next) {
-  console.log(error.message);
-  next();
-}
-app.use(custommiddleware);
-app.use(errormiddleware);
 app.use(morgan("dev"));
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/user", (req, res) => res.send("USER!!"));
 
 app.listen(port, () =>
   console.log(`Server running at http://127.0.0.1:${port}`)
